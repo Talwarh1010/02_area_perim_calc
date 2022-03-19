caedb31e5b92aa095362bb456ad7c0d0cc77ab6e
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-03-19 17:54:14 +1300

    Add files via upload

--- Area_Perim_calc task.py
+++ Area_Perim_calc task.py
@@ -1,1 +1,41 @@
-# ask the user for the width
+# functions go here
+
+# checks input is a number more than zero
+def num_check(question):
+    valid = False
+    while not valid:
+        
+        error = ("Please enter a number that is not zero.")
+            
+        try:
+            
+            # asks the user to enter a number
+            response = float(input(question))
+            
+            # checks number  is more than zero
+            if response > 0:
+                return response
+                
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+            
+        except ValueError:
+            print(error)
+            
+            
+            
+# main routine goes here
+width = num_check("width: ")
+height = num_check("height: ") 
+
+# calculate area ( width x height)
+area = width * height
+print(area)
+# calculate perimeter
+Perimeter = width * 2 + height * 2
+print( " The perimeter is, {} square units". format(Perimeter))
+print(" The area is, {} units".format(area))
+
+
