2b9670362c3231b6922d01743878990a0c1b9cb6
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-03-19 17:38:09 +1300

    Add files via upload

--- 01_looping_basic.py
+++ 01_looping_basic.py
@@ -1,22 +1,35 @@
-# checks that the users enter a number that is more than zero
-valid = False
-while not valid:
-      
-    error = ("Please enter a number that is not zero.")
+# functions go here
+
+# checks input is a number more than zero
+def num_check(question):
+    valid = False
+    while not valid:
         
-    try:
-        
-        # asks the user to enter a number
-        response = float(input("Enter a Number: "))
-        
-        # checks number  is more than zero
-        if response > 0:
-            valid = True
+        error = ("Please enter a number that is not zero.")
             
-        # outputs error if input is invalid
-        else:
+        try:
+            
+            # asks the user to enter a number
+            response = float(input(question))
+            
+            # checks number  is more than zero
+            if response > 0:
+                return response
+                
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+            
+        except ValueError:
             print(error)
-            print()
-        
-    except ValueError:
-       print(error)     
+            
+            
+            
+# main routine goes here
+width = num_check("width: ")
+height = num_check("height: ")  
+print() 
+print("Width", width)
+print("Height", height)
+print()
